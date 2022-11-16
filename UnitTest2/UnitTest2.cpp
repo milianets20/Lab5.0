bdb54deb8547daf77d336f2db60cf4988ddc5aff
milianets20 <marian.milianets.ri.2022@lpnu.ua>
2022-11-16 23:46:13 +0200

    second test

---
+++ UnitTest2/UnitTest2.cpp
@@ -0,0 +1,21 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab5.1/Lab5.1.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest2
+{
+	TEST_CLASS(UnitTest2)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1);
+	};
+	inline void UnitTest2::TestMethod1()
+	{
+		int q;
+		q = h(-1, 0);
+		Assert::AreEqual(q, -1);
+	}
+}
