459cb4e0d064424e631de2461e320dea756d9b24
milianets20 <marian.milianets.ri.2022@lpnu.ua>
2022-11-16 22:50:14 +0200

    dry

---
+++ Lab5.1/Lab5.1.cpp
@@ -0,0 +1,18 @@
+﻿// Lab 5_1
+#include <iostream>
+#include <cmath>
+using namespace std;
+double h(const double x, const double y); // прототип
+int main()
+{
+	double p, q;
+		cout << "p = "; cin >> p;
+	cout << "q = "; cin >> q;
+	double c = pow(h(abs(p-q), sqrt(abs(p-q))), 2)+h(1,q*q-p*p);
+	cout << "c = " << c << endl;
+	return 0;
+}
+double h(const double x, const double y) // визначення
+{
+	return (pow(y,2)-sqrt(pow(x,2)+pow(y,2))/(1+pow(x,2)*pow(y,2)));
+}
